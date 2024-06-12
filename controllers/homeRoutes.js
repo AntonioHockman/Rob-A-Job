2bdc9da9f98c7b9e07e217658999b729b65b3a3b
mackemo <mackenzielmoore14@gmail.com>
2024-06-12 19:41:11 -0400

    created homeroutes for rendering jobs to homepage, users on user page, and rediredirecting if user is logged in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,32 +1,40 @@
 const router = require('express').Router();
-const { User } = require('../models');
-//const withAuth = require('../utils/auth');
+const { User } = require('../models/User');
+const { Job } = require('../models/Job');
+const withAuth = require('../utils/auth');
+
 
 router.get('/', async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    try {
+        const jobData = await Job.findAll();
+        return res.render('homepage', jobData)
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    } catch (err) {
+        res.status(500).json({err: 'Internal Server Error'});
+    }
+});
 
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/user', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC']],
+    })
+        return res.render('users', userData)
+
+    } catch (err) {
+    res.status(500).json({err: 'Internal Server Error'});
+    }
 });
 
 router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
+    if (req.session.logged_in) {
+        res.redirect('/');
     return;
-  }
+    }
 
-  res.render('login');
+    res.render('login');
 });
+
 
 module.exports = router;

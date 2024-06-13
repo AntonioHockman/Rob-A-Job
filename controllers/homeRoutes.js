15ce8e2ffad1bcde9548dda7bf194b8a87e54a81
mackemo <mackenzielmoore14@gmail.com>
2024-06-12 20:01:44 -0400

    updated homeroutes with finding single job and user by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { User } = require('../models/User');
 const { Job } = require('../models/Job');
 const withAuth = require('../utils/auth');
 
-
+// rendering homepage with all jobs
 router.get('/', async (req, res) => {
     try {
         const jobData = await Job.findAll();
@@ -14,6 +14,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+// finding single job by id
+router.get('/:id', async (req, res) => {
+    try {
+      const jobData = await Job.findByPk(req.params.id, {
+        include: [{ model: User }]
+      })
+  
+      if (!jobData) {
+        res.status(404).json({message: 'No job with that id'});
+        return;
+      }
+      res.status(200).json(jobData);
+    } catch (error) {
+      res.status(500).json({message: 'Internal Server Error'});
+    }
+  });
+
+// render user page with all users
 router.get('/user', withAuth, async (req, res) => {
     try {
         const userData = await User.findAll({
@@ -27,6 +45,24 @@ router.get('/user', withAuth, async (req, res) => {
     }
 });
 
+// finding single user by id
+router.get('/user/:id', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id, {
+        include: [{ model: Job }]
+      })
+  
+      if (!userData) {
+        res.status(404).json({message: 'No user with that id'});
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (error) {
+      res.status(500).json({message: 'Internal Server Error'});
+    }
+  });
+
+//  rendering login page
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/');

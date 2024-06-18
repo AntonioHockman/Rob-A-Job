2c62c47c164c1240d89817d7b25efff7d7cde9b6
AntonioHockman <150075660+AntonioHockman@users.noreply.github.com>
2024-06-17 20:53:11 -0500

    updated connection.js for render

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres'
-  }
-);
-
+let sequelize;
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
+}
 module.exports = sequelize;
+
 // Above we establish  our connection from values  out of our .env file 
